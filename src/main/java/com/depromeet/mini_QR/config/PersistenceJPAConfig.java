74a8d7238039db6b9cbad354ad350ade1d35e589
MaKyuSeok <mks502@naver.com>
2019-04-15 21:09:11 +0900

    JPA Setting End

--- src/main/java/com/depromeet/mini_QR/config/PersistenceJPAConfig.java
+++ src/main/java/com/depromeet/mini_QR/config/PersistenceJPAConfig.java
@@ -1,69 +1,69 @@
-//package com.depromeet.mini_QR.config;
-//
-//import java.util.Properties;
-//
-//import javax.persistence.EntityManagerFactory;
-//import javax.sql.DataSource;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.PropertySource;
-//import org.springframework.core.env.Environment;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-//import org.springframework.jdbc.datasource.DriverManagerDataSource;
-//import org.springframework.orm.jpa.JpaTransactionManager;
-//import org.springframework.orm.jpa.JpaVendorAdapter;
-//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-//import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-//import org.springframework.transaction.PlatformTransactionManager;
-//import org.springframework.transaction.annotation.EnableTransactionManagement;
-//
-//@Configuration
-//@EnableJpaRepositories("com.depromeet.mini_QR.domain")
-//@EnableTransactionManagement
-//@PropertySource("classpath:/com/depromeet/mini_QR/config/db.properties")
-//public class PersistenceJPAConfig {
-//	
-//	@Autowired
-//    Environment env;
-//	
-//	@Bean
-//	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-//		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-//		em.setDataSource(dataSource());
-//		em.setPackagesToScan(new String[] { "com.depromeet.mini_QR.domain" });
-//	
-//		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-//		em.setJpaVendorAdapter(vendorAdapter);
-//		em.setJpaProperties(additionalProperties());
-//		
-//		return em;
-//	}
-//	
-//	@Bean
-//	public DataSource dataSource() {
-//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//		dataSource.setDriverClassName( env.getProperty("db.dname") );
-//		dataSource.setUrl( env.getProperty("db.url") );
-//		dataSource.setUsername( env.getProperty("db.user") );
-//		dataSource.setPassword( env.getProperty("db.password") );
-//		return dataSource;
-//	}
-//	
-//	@Bean
-//	public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
-//		JpaTransactionManager transactionManager = new JpaTransactionManager();
-//		transactionManager.setEntityManagerFactory(emf);
-//	
-//		return transactionManager;
-//	}
-//	
-//	Properties additionalProperties() {
-//		Properties properties = new Properties();
-//		properties.setProperty("hibernate.hbm2ddl.auto", "update");
-//		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-//		properties.setProperty("hibernate.enable_lazy_load_no_trans", "true");
-//		return properties;
-//	}
-//}
+package com.depromeet.mini_QR.config;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories("com.depromeet.mini_QR.domain")
+@EnableTransactionManagement
+@PropertySource("classpath:/com/depromeet/mini_QR/config/db.properties")
+public class PersistenceJPAConfig {
+	
+	@Autowired
+    Environment env;
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setPackagesToScan(new String[] { "com.depromeet.mini_QR.domain" });
+	
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(additionalProperties());
+		
+		return em;
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName( env.getProperty("db.dname") );
+		dataSource.setUrl( env.getProperty("db.url") );
+		dataSource.setUsername( env.getProperty("db.user") );
+		dataSource.setPassword( env.getProperty("db.password") );
+		return dataSource;
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+	
+		return transactionManager;
+	}
+	
+	Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		properties.setProperty("hibernate.enable_lazy_load_no_trans", "true");
+		return properties;
+	}
+}
--- src/main/webapp/WEB-INF/jsp/home.jsp
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -9,6 +9,6 @@
 	Hello world!  
 </h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+<P>  GooooooooooooooooooooooooD The time on the server is ${serverTime}. </P>
 </body>
 </html>
