8681e5af6b76f76d4a837b622e326d52cf8d9d70
kimshinje <shinjehere@gmail.com>
2019-04-16 17:03:37 +0900

    create ChatRoom Entity

---
+++ src/main/java/com/depromeet/mini_QR/domain/entity/ChatRoom.java
@@ -0,0 +1,24 @@
+package com.depromeet.mini_QR.domain.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatRoom {
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	String chatId;
+	String chatTitle;
+	String fullURL;
+	String shortURL;
+}
