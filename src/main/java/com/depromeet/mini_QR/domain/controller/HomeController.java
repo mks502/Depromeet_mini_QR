f525a05415bbc7acbd45d4f57895804a4429f0dd
kimshinje <shinjehere@gmail.com>
2019-04-18 04:48:50 +0900

    chat POST 메소드 추

--- src/main/java/com/depromeet/mini_QR/domain/controller/HomeController.java
+++ src/main/java/com/depromeet/mini_QR/domain/controller/HomeController.java
@@ -5,11 +5,20 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-@Controller
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class HomeController {
 	
 	@GetMapping(value = "/")
     public String index(HttpServletRequest req, HttpServletResponse res) {
         return "home";
     }
+	
+	@PostMapping(value = "/chat/{chatId}")
+	public String setChatName(HttpServletRequest req, HttpServletResponse res){
+		String chatName = req.getParameter("chatName");
+		
+		return chatName;
+	}
 }
