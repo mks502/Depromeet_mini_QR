1ddd31f4f44795112f2e158dbfe3eed5e59373e6
kimshinje <shinjehere@gmail.com>
2019-04-18 16:46:31 +0900

    initialize swagger config, complete test working

---
+++ src/main/java/com/depromeet/mini_QR/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.depromeet.mini_QR.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@EnableWebMvc
+public class SwaggerConfig {                                    
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())  // 현재 RequestMapping으로 할당된 모든 URL 리스트를 추출             
+          .paths(PathSelectors.any())  // 그중 /api/** 인 URL들만 필터링                         
+          .build();                                           
+    }
+    
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+          .addResourceLocations("classpath:/META-INF/resources/");
+     
+        registry.addResourceHandler("/webjars/**")
+          .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
