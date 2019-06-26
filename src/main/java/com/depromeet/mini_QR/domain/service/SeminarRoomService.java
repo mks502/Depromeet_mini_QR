0c3fdd1dbf3244e85548d0afd278d70d9f52d9d9
kimshinje <shinjehere@gmail.com>
2019-06-26 17:46:41 +0900

    코드정리 및 주석생성

--- src/main/java/com/depromeet/mini_QR/domain/service/SeminarRoomService.java
+++ src/main/java/com/depromeet/mini_QR/domain/service/SeminarRoomService.java
@@ -19,34 +19,51 @@ public class SeminarRoomService {
 	@Autowired
 	SeminarRoomRepository seminarRoomRepository;
 	
+	/**
+	 * 새로운 SeminarRoom 채팅방 생성 
+	 * @param seminarRoom
+	 * @return
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
 	public SeminarRoomDto createSeminar(SeminarRoom seminarRoom) throws MalformedURLException, IOException {
 		seminarRoomRepository.save(seminarRoom);
-		String title= seminarRoom.getSeminarTitle();
-		SeminarRoom newSeminar =seminarRoomRepository.findBySeminarTitle(title);
-		String full= "/seminar/";
-		String seminarid= Long.toString(newSeminar.getSeminarId());
+		String seminarTitle= seminarRoom.getSeminarTitle();
+		SeminarRoom newSeminarRoom =seminarRoomRepository.findBySeminarTitle(seminarTitle);
+		String fullURL= "/seminar/";
+		String seminarId= Long.toString(newSeminarRoom.getSeminarId());
 		String longURL = "http://15.164.104.40:8081/mini_QR/";
-//		String longURL = "http://lvh.me/mini_QR/";
 		
-		full= full.concat(seminarid);
-		newSeminar.setFullURL(full);
-		longURL = longURL.concat(full);
-		newSeminar.setShortURL(createShortUrl(longURL));
-		seminarRoomRepository.save(newSeminar);
+		fullURL = fullURL.concat(seminarId);
+		newSeminarRoom.setFullURL(fullURL);
+		longURL = longURL.concat(fullURL);
+		newSeminarRoom.setShortURL(createShortUrl(longURL));
+		seminarRoomRepository.save(newSeminarRoom);
 		
-		return newSeminar.toDto();
+		return newSeminarRoom.toDto();
 	}
-	
-	// bit.ly api를 사용하여 fullUrl을 shortUrl로 변환 
+	 
+	/**
+	 * bit.ly 라이브러리를 사용하여 fullUrl을 shortUrl로 변환
+	 * @param fullUrl
+	 * @return shortUrl
+	 * @throws MalformedURLException
+	 * @throws IOException
+	 */
 	public String createShortUrl(String fullUrl) throws MalformedURLException, IOException{
-		
 		Url url = as("o_1duq7b20be", "R_01bd87bf046e49cd93816dd681925740").call(shorten(fullUrl));
 		String shortUrl = url.getShortUrl();
 		
 		return shortUrl;
 	}
-	public SeminarRoomDto findSeminar(Long id) {
-		SeminarRoom seminarRoom= seminarRoomRepository.findBySeminarId(id);
+	
+	/**
+	 * id로 seminarRoom 조회 
+	 * @param seminarId
+	 * @return seminarRoomDto 
+	 */
+	public SeminarRoomDto findSeminar(Long seminarId) {
+		SeminarRoom seminarRoom= seminarRoomRepository.findBySeminarId(seminarId);
 		return seminarRoom.toDto();
 	}
 }
